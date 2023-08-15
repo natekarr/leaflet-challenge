0863aabfa960af9912a45c6ccae60da9fc21b392
natekarr <Nathan>
2023-08-15 16:15:29 -0500

    push

--- static/js/logic.js
+++ static/js/logic.js
@@ -11,69 +11,66 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 
 // Function to determine marker size based on earthquake magnitude
 function getMarkerSize(magnitude) {
-    return Math.sqrt(magnitude) * 5;
+  return Math.sqrt(magnitude) * 5;
 }
 
 // Function to determine marker color based on earthquake depth
 function getMarkerColor(depth) {
-    const colors = ['#00FF00', '#32CD32', '#FFFF00', '#FFA500', '#FF4500', '#FF0000'];
-    const breakpoints = [-10, 10, 30, 50, 70, 90];
-    
-    let colorIndex = 0;
-    for (let i = 0; i < breakpoints.length; i++) {
-      if (depth <= breakpoints[i]) {
-        colorIndex = i;
-        break;
-      }
+  const colors = ['#00FF00', '#32CD32', '#FFFF00', '#FFA500', '#FF4500', '#FF0000'];
+  const breakpoints = [-10, 10, 30, 50, 70, 90];
+  
+  let colorIndex = 0;
+  for (let i = 0; i < breakpoints.length; i++) {
+    if (depth <= breakpoints[i]) {
+      colorIndex = i;
+      break;
     }
-    
-    return colors[colorIndex];
   }
+  
+  return colors[colorIndex];
+}
 
-// Fetch GeoJSON data
-fetch(geoJSONUrl)
-  .then(response => response.json())
-  .then(data => {
-    // Create markers for each earthquake
-    L.geoJSON(data.features, {
-      pointToLayer: (feature, latlng) => {
-        const magnitude = feature.properties.mag;
-        const depth = feature.geometry.coordinates[2];
-        const markerSize = getMarkerSize(magnitude);
-        const markerColor = getMarkerColor(depth);
+// Use D3.js to fetch GeoJSON data
+d3.json(geoJSONUrl).then(data => {
+  // Create markers for each earthquake
+  L.geoJSON(data.features, {
+    pointToLayer: (feature, latlng) => {
+      const magnitude = feature.properties.mag;
+      const depth = feature.geometry.coordinates[2];
+      const markerSize = getMarkerSize(magnitude);
+      const markerColor = getMarkerColor(depth);
 
-        // Create a circle marker
-        return L.circleMarker(latlng, {
-          radius: markerSize,
-          fillColor: markerColor,
-          color: '#000',
-          weight: 1,
-          opacity: 1,
-          fillOpacity: 0.8
-        }).bindPopup(`<strong>Location:</strong> ${feature.properties.place}<br><strong>Magnitude:</strong> ${magnitude}<br><strong>Depth:</strong> ${depth}`);
-      }
-    }).addTo(map);
+      // Create a circle marker
+      return L.circleMarker(latlng, {
+        radius: markerSize,
+        fillColor: markerColor,
+        color: '#000',
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+      }).bindPopup(`<strong>Location:</strong> ${feature.properties.place}<br><strong>Magnitude:</strong> ${magnitude}<br><strong>Depth:</strong> ${depth}`);
+    }
+  }).addTo(map);
 
-    // Create a legend
-    const legend = L.control({ position: 'bottomright' });
+  // Create a legend
+  const legend = L.control({ position: 'bottomright' });
 
-    legend.onAdd = () => {
-        const div = L.DomUtil.create('div', 'info legend');
-        div.innerHTML = '<h4>Depth</h4>'; // Title
-        const depths = [-10, 10, 30, 50, 70, 90];
-        const labels = [];
-      
-        for (let i = 0; i < depths.length; i++) {
-          const colorSquare = `<span class="color-square" style="background: ${getMarkerColor((depths[i] + depths[i - 1]) / 2)};"></span>`;
-          labels.push(
-            colorSquare +
-            depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+'));
-        }
-      
-        div.innerHTML += labels.join('<br>');
-        return div;
-      };
+  legend.onAdd = () => {
+    const div = L.DomUtil.create('div', 'info legend');
+    div.innerHTML = '<h4>Depth</h4>'; // Title
+    const depths = [-10, 10, 30, 50, 70, 90];
+    const labels = [];
 
-    legend.addTo(map);
-  })
-  .catch(error => console.error("Error fetching data:", error));
+    for (let i = 0; i < depths.length; i++) {
+      const colorSquare = `<span class="color-square" style="background: ${getMarkerColor((depths[i] + depths[i - 1]) / 2)};"></span>`;
+      labels.push(
+        colorSquare +
+        depths[i] + (depths[i + 1] ? '&ndash;' + depths[i + 1] + '<br>' : '+'));
+    }
+
+    div.innerHTML += labels.join('<br>');
+    return div;
+  };
+
+  legend.addTo(map);
+}).catch(error => console.error("Error fetching data:", error));
